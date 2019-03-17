00bf495cebb2275937233cb8ecd8d6e959772c07
Filip Stål <filsta@kth.se>
2019-03-17 02:43:48 +0100

    Minor cleanup

--- lab2/test.py
+++
@@ -1,9 +0,0 @@
-"""
-
-    #For testing - printing all nodes
-    def list_print(self):
-        node = self.__firstnode
-        while node:
-            print(node.value)
-            node = node.next
-"""
